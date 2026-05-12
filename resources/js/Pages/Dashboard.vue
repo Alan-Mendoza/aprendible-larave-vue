e92f9ebd184bc0b8217f0cfca77a3e66a36d703d
Alan Mendoza <alanrodrigo.mg@gmail.com>
2026-05-12 12:49:50 -0400

    cambios de colore y manejo de Link Nav

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,7 +15,7 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 dark:text-gray-200 p-5 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Link :href="route('chirps.index')">Chirps</Link>
+                    Dashboard Content
                 </div>
             </div>
         </div>
