ac0c343a41ab6587058a25878e44f22a36e14c95
Alan Mendoza <alanrodrigo.mg@gmail.com>
2026-05-12 16:17:44 -0400

    Envio de formulario por Chirps sin Inertia

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,19 +1,33 @@
 <script setup>
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import InputError from '@/Components/InputError.vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import axios from 'axios';
 import { ref } from 'vue';
 
 const message = ref('');
+const errors = ref({});
+const processing = ref(false);
 
 function submit() {
+    processing.value = true;
     axios.post(route('chirps.store'), { message: message.value })
         .then((res) => {
             console.log(res.data);
             message.value = '';
+            errors.value = {};
         }).catch((error) => {
-            console.error(error.response.data.message);
+            // console.log(error.response.status)
+            if (error.response.status === 422) {
+                errors.value = error.response.data.errors;
+                return; // Salir de la función para evitar mostrar el mensaje de error genérico
+            }
+            // console.log(error.response.data.errors);
+            // errors.value = error.response.data.errors;
+            console.error(error.response.data.message); // Super importante para ver el mensaje de error específico en la consola
+        }).finally(() => {
+            processing.value = false;
         });
 }
 
@@ -36,8 +50,12 @@ defineProps(['title', 'subtitle']);
                 <div class="bg-white dark:bg-gray-800 dark:text-gray-200 overflow-hidden shadow-xl sm:rounded-lg p-5">
                     <form @submit.prevent="submit">
                         <textarea v-model="message" placeholder="Que estas pensando ?" class="block w-full rounded-md border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 focus:border-emerald-500 focus:ring-emerald-500"></textarea>
-                        <PrimaryButton>
-                            Chirp
+                        <!-- <div v-if="errors.message" class="text-red-500 text-sm mt-1">
+                            {{ errors.message && errors.message[0] }}
+                        </div> -->
+                        <InputError :message="errors.message && errors.message[0]" class="mt-2" />
+                        <PrimaryButton :disabled="processing">
+                            {{ processing ? 'Enviando...' : 'Chirps' }}
                         </PrimaryButton>
                     </form>
                 </div>
